935beae1800b84b755b5249d30aefb7bd640f69a
Mario Leandro <marioleandro34@gmail.com>
2021-04-23 12:37:43 -0300

    feat: add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,6 +37,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
 
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
